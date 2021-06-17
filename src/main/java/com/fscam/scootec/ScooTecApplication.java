0b9f611c5b5c275845ad2fe0d1c0e61da4a9dedc
Aike Errenst <aer@colamda.de>
2021-06-17 14:01:28 +0200

    spring is da

---
+++ src/main/java/com/fscam/scootec/ScooTecApplication.java
@@ -0,0 +1,13 @@
+package com.fscam.scootec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScooTecApplication.class, args);
+	}
+
+}
