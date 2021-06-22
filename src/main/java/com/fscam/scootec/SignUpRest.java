8c85c596938b88c3cbef43926082ecf7cdc43ca8
Aike Errenst <aer@colamda.de>
2021-06-22 10:50:03 +0200

    frontend kommt beim backend an. PARTY!!!!

--- src/main/java/com/fscam/scootec/SignUpRest.java
+++ src/main/java/com/fscam/scootec/SignUpRest.java
@@ -1,12 +1,13 @@
 package com.fscam.scootec;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SignUpRest {
     @PostMapping("sign-up")
-    public void createAccount(Account account) {
-
+    public void createAccount(@RequestBody Account account) {
+        System.out.println(account.getVorname());
     }
 }
